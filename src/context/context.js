5795dc3d938d8e160f60ff2d992700de7e792519
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-11-18 12:58:52 +0100

    moved file with actions for useReducer hook to the reducer folder

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 import reducer from "../reducer/reducer";
-import * as actionsModule from "../actions";
+import * as actions from "../reducer/actions";
 
 const mainUrl = "https://api.edamam.com/api/recipes/v2?type=public";
 
@@ -41,108 +41,104 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const rotateStar = () => {
-    dispatch({ type: actionsModule.ROTATE_STAR });
+    dispatch({ type: actions.ROTATE_STAR });
     setTimeout(() => {
-      dispatch({ type: actionsModule.REFRESH_STAR });
+      dispatch({ type: actions.REFRESH_STAR });
     }, 300);
   };
 
   const handleError = (action) => {
-    dispatch({ type: actionsModule.HANDLE_ERROR, payload: action });
+    dispatch({ type: actions.HANDLE_ERROR, payload: action });
   };
 
   const loadingToFalse = () => {
-    dispatch({ type: actionsModule.GET_RECIPES_SUCCESS });
+    dispatch({ type: actions.GET_RECIPES_SUCCESS });
   };
   const setNextPageLoading = () => {
-    dispatch({ type: actionsModule.NEXT_PAGE_LOADING });
+    dispatch({ type: actions.NEXT_PAGE_LOADING });
   };
   const createQuery = (someQuery) => {
-    dispatch({ type: actionsModule.CREATE_A_QUERY, payload: someQuery });
+    dispatch({ type: actions.CREATE_A_QUERY, payload: someQuery });
   };
   const clearQuery = () => {
-    dispatch({ type: actionsModule.CLEAR_THE_QUERY });
+    dispatch({ type: actions.CLEAR_THE_QUERY });
   };
 
   const handleQuery = (lastQuery) => {
-    dispatch({ type: actionsModule.HANDLE_LAST_QUERY, payload: lastQuery });
+    dispatch({ type: actions.HANDLE_LAST_QUERY, payload: lastQuery });
   };
 
   const changeThePage = (number) => {
-    dispatch({ type: actionsModule.CHANGE_THE_PAGE, payload: number });
+    dispatch({ type: actions.CHANGE_THE_PAGE, payload: number });
   };
 
   const changeThePath = (newPath) => {
-    dispatch({ type: actionsModule.CHANGE_CURRENT_PATH, payload: newPath });
+    dispatch({ type: actions.CHANGE_CURRENT_PATH, payload: newPath });
   };
 
   const newPath = (newPath) => {
-    dispatch({ type: actionsModule.SET_PATH, payload: newPath });
+    dispatch({ type: actions.SET_PATH, payload: newPath });
   };
 
   const newQueryPath = (newPath) => {
-    dispatch({ type: actionsModule.SET_QUERY_PATH, payload: newPath });
+    dispatch({ type: actions.SET_QUERY_PATH, payload: newPath });
   };
   const newLocalStrPath = (newPath) => {
-    dispatch({ type: actionsModule.SET_LOCALSTR_PATH, payload: newPath });
+    dispatch({ type: actions.SET_LOCALSTR_PATH, payload: newPath });
   };
 
   const setRecipe = (newRecipe) => {
     dispatch({
-      type: actionsModule.GET_RECIPE,
+      type: actions.GET_RECIPE,
       payload: !newRecipe ? "recipe" : newRecipe,
     });
   };
   const updateLocalStrRecipes = (newRecipe) => {
     dispatch({
-      type: actionsModule.UPDATE_LOCALSTR_RECIPES,
+      type: actions.UPDATE_LOCALSTR_RECIPES,
       payload: newRecipe,
     });
   };
-  const showHideModal = () => {
-    dispatch({ type: actionsModule.SHOW_HIDE_MODAL, payload: !state.isModal });
+  const handleModal = () => {
+    dispatch({ type: actions.HANDLE_MODAL, payload: !state.isModal });
   };
   const saveEmail = (email) => {
-    dispatch({ type: actionsModule.SAVE_EMAIL, payload: email });
+    dispatch({ type: actions.SAVE_EMAIL, payload: email });
   };
 
   const changePreferences = (type, change) => {
     type === "calories" &&
-      dispatch({ type: actionsModule.CHANGE_COLORIES, payload: change });
-    type === "diet" &&
-      dispatch({ type: actionsModule.CHANGE_DIET, payload: change });
+      dispatch({ type: actions.CHANGE_CALORIES, payload: change });
+    type === "diet" && dispatch({ type: actions.CHANGE_DIET, payload: change });
     type === "health" &&
-      dispatch({ type: actionsModule.CHANGE_HEALTH, payload: change });
-    type === "meal" &&
-      dispatch({ type: actionsModule.CHANGE_MEAL, payload: change });
+      dispatch({ type: actions.CHANGE_HEALTH, payload: change });
+    type === "meal" && dispatch({ type: actions.CHANGE_MEAL, payload: change });
     type === "cuisine" &&
-      dispatch({ type: actionsModule.CHANGE_CUISINE, payload: change });
-    type === "dish" &&
-      dispatch({ type: actionsModule.CHANGE_DISH, payload: change });
+      dispatch({ type: actions.CHANGE_CUISINE, payload: change });
+    type === "dish" && dispatch({ type: actions.CHANGE_DISH, payload: change });
     type === "minInput" &&
-      dispatch({ type: actionsModule.CHANGE_MININPUT, payload: change });
+      dispatch({ type: actions.CHANGE_MININPUT, payload: change });
     type === "maxInput" &&
-      dispatch({ type: actionsModule.CHANGE_MAXINPUT, payload: change });
+      dispatch({ type: actions.CHANGE_MAXINPUT, payload: change });
   };
 
   React.useEffect(() => {
     !state.email &&
       dispatch({
-        type: actionsModule.SHOW_HIDE_MODAL,
+        type: actions.HANDLE_MODAL,
         payload: !state.isModal,
       });
     // eslint-disable-next-line
   }, [state.email]);
 
   React.useEffect(() => {
-    state.query &&
-      dispatch({ type: actionsModule.CHANGE_THE_PAGE, payload: 1 });
+    state.query && dispatch({ type: actions.CHANGE_THE_PAGE, payload: 1 });
   }, [state.query]);
 
   React.useEffect(() => {
     (state.minInput > 0 || state.maxInput < 1000) &&
       dispatch({
-        type: actionsModule.CHANGE_COLORIES,
+        type: actions.CHANGE_CALORIES,
         payload: `${state.minInput}-${state.maxInput}`,
       });
   }, [state.minInput, state.maxInput]);
@@ -174,18 +170,18 @@ const AppProvider = ({ children }) => {
 
   const fetchRecipes = async () => {
     const saveQuery = (someQuery) => {
-      dispatch({ type: actionsModule.HANDLE_LAST_QUERY, payload: someQuery });
+      dispatch({ type: actions.HANDLE_LAST_QUERY, payload: someQuery });
 
       localStorage.setItem("lastQuery", JSON.stringify(someQuery));
     };
 
     const nextPage = (someData, someLink) => {
       if (someLink !== 1) {
-        dispatch({ type: actionsModule.SET_NEXT_PAGE, payload: "" });
+        dispatch({ type: actions.SET_NEXT_PAGE, payload: "" });
         localStorage.setItem("nextPage", JSON.stringify(""));
       } else {
         dispatch({
-          type: actionsModule.SET_NEXT_PAGE,
+          type: actions.SET_NEXT_PAGE,
           payload: someData._links.next.href,
         });
         localStorage.setItem(
@@ -197,13 +193,13 @@ const AppProvider = ({ children }) => {
 
     const pageNData = (p, d) => {
       localStorage.setItem("page", JSON.stringify(p));
-      dispatch({ type: actionsModule.GET_RECIPES_DATA, payload: d });
+      dispatch({ type: actions.GET_RECIPES_DATA, payload: d });
       localStorage.setItem("recipes-data", JSON.stringify(d));
     };
 
     try {
       if (state.page === 1 && state.query) {
-        dispatch({ type: actionsModule.GET_RECIPES_BEGIN });
+        dispatch({ type: actions.GET_RECIPES_BEGIN });
 
         const response = await fetch(url);
         const data = await response.json();
@@ -211,7 +207,7 @@ const AppProvider = ({ children }) => {
         const nextPageLink = Object.keys(data._links).length;
 
         dispatch({
-          type: actionsModule.CHANGE_THE_PAGE,
+          type: actions.CHANGE_THE_PAGE,
           payload: state.page + 1,
         });
 
@@ -223,20 +219,20 @@ const AppProvider = ({ children }) => {
         nextPage(data, nextPageLink);
 
         dispatch({
-          type: actionsModule.GET_ALL_RECIPES,
+          type: actions.GET_ALL_RECIPES,
           payload: recipesArray,
         });
         localStorage.setItem("recipes", JSON.stringify([...recipesArray]));
 
         data.hits.length === 0 &&
-          dispatch({ type: actionsModule.HANDLE_ERROR, payload: true });
+          dispatch({ type: actions.HANDLE_ERROR, payload: true });
 
         dispatch({
-          type: actionsModule.CHANGE_CURRENT_PATH,
+          type: actions.CHANGE_CURRENT_PATH,
           payload: window.location.pathname,
         });
 
-        dispatch({ type: actionsModule.GET_RECIPES_SUCCESS });
+        dispatch({ type: actions.GET_RECIPES_SUCCESS });
       }
 
       if (state.page >= 1 && !state.query) {
@@ -250,7 +246,7 @@ const AppProvider = ({ children }) => {
         nextPage(data, nextPageLink);
 
         dispatch({
-          type: actionsModule.GET_ALL_RECIPES,
+          type: actions.GET_ALL_RECIPES,
           payload: [...state.recipes, ...recipesArray],
         });
 
@@ -260,8 +256,8 @@ const AppProvider = ({ children }) => {
         );
 
         data.hits.length === 0 &&
-          dispatch({ type: actionsModule.HANDLE_ERROR, payload: true });
-        dispatch({ type: actionsModule.NEXT_PAGE_LOADED });
+          dispatch({ type: actions.HANDLE_ERROR, payload: true });
+        dispatch({ type: actions.NEXT_PAGE_LOADED });
       }
     } catch (error) {
       console.log(error);
@@ -285,7 +281,7 @@ const AppProvider = ({ children }) => {
         newLocalStrPath,
         setRecipe,
         updateLocalStrRecipes,
-        showHideModal,
+        handleModal,
         saveEmail,
         changePreferences,
         handleError,
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,140 +1,154 @@
-import * as actionsModule from "../actions";
+import * as actions from "./actions";
 
 const reducer = (state, action) => {
-  if (action.type === actionsModule.GET_RECIPES_BEGIN) {
+  if (action.type === actions.GET_RECIPES_BEGIN) {
     return {
       ...state,
       isLoading: true,
     };
   }
-  if (action.type === actionsModule.GET_RECIPES_SUCCESS) {
+
+  if (action.type === actions.GET_RECIPES_SUCCESS) {
     return {
       ...state,
       isLoading: false,
     };
   }
 
-  if (action.type === actionsModule.HANDLE_ERROR) {
+  if (action.type === actions.HANDLE_ERROR) {
     return {
       ...state,
       isError: action.payload,
     };
   }
 
-  if (action.type === actionsModule.ROTATE_STAR) {
+  if (action.type === actions.ROTATE_STAR) {
     return {
       ...state,
       rotate: true,
     };
   }
-  if (action.type === actionsModule.REFRESH_STAR) {
+
+  if (action.type === actions.REFRESH_STAR) {
     return {
       ...state,
       rotate: false,
     };
   }
 
-  if (action.type === actionsModule.NEXT_PAGE_LOADING) {
+  if (action.type === actions.NEXT_PAGE_LOADING) {
     return {
       ...state,
       nextPageLoading: true,
     };
   }
-  if (action.type === actionsModule.NEXT_PAGE_LOADED) {
+
+  if (action.type === actions.NEXT_PAGE_LOADED) {
     return {
       ...state,
       nextPageLoading: false,
     };
   }
 
-  if (action.type === actionsModule.CREATE_A_QUERY) {
+  if (action.type === actions.CREATE_A_QUERY) {
     return {
       ...state,
       query: action.payload,
     };
   }
-  if (action.type === actionsModule.CLEAR_THE_QUERY) {
+
+  if (action.type === actions.CLEAR_THE_QUERY) {
     return {
       ...state,
       query: "",
     };
   }
 
-  if (action.type === actionsModule.HANDLE_LAST_QUERY) {
+  if (action.type === actions.HANDLE_LAST_QUERY) {
     return {
       ...state,
       lastQuery: action.payload,
     };
   }
 
-  if (action.type === actionsModule.CHANGE_THE_PAGE) {
+  if (action.type === actions.CHANGE_THE_PAGE) {
     return {
       ...state,
       page: action.payload,
     };
   }
-  if (action.type === actionsModule.SET_NEXT_PAGE) {
+
+  if (action.type === actions.SET_NEXT_PAGE) {
     return {
       ...state,
       nextPage: action.payload,
     };
   }
-  if (action.type === actionsModule.SET_PATH) {
+
+  if (action.type === actions.SET_PATH) {
     return {
       ...state,
       path: action.payload,
     };
   }
-  if (action.type === actionsModule.SET_QUERY_PATH) {
+
+  if (action.type === actions.SET_QUERY_PATH) {
     return {
       ...state,
       queryPath: action.payload,
     };
   }
-  if (action.type === actionsModule.SET_LOCALSTR_PATH) {
+
+  if (action.type === actions.SET_LOCALSTR_PATH) {
     return {
       ...state,
       localStrPath: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_CURRENT_PATH) {
+
+  if (action.type === actions.CHANGE_CURRENT_PATH) {
     return {
       ...state,
       currentPath: action.payload,
     };
   }
-  if (action.type === actionsModule.GET_RECIPES_DATA) {
+
+  if (action.type === actions.GET_RECIPES_DATA) {
     return {
       ...state,
       recipesData: action.payload,
     };
   }
-  if (action.type === actionsModule.GET_RECIPE) {
+
+  if (action.type === actions.GET_RECIPE) {
     return {
       ...state,
       recipe: action.payload,
     };
   }
-  if (action.type === actionsModule.GET_ALL_RECIPES) {
+
+  if (action.type === actions.GET_ALL_RECIPES) {
     return {
       ...state,
       recipes: action.payload,
     };
   }
-  if (action.type === actionsModule.UPDATE_LOCALSTR_RECIPES) {
+
+  if (action.type === actions.UPDATE_LOCALSTR_RECIPES) {
     return {
       ...state,
       localStrRecipes: action.payload,
     };
   }
 
-  if (action.type === actionsModule.SHOW_HIDE_MODAL) {
+  if (action.type === actions.HANDLE_MODAL) {
     return {
       ...state,
       isModal: action.payload,
     };
   }
-  if (action.type === actionsModule.SAVE_EMAIL) {
+
+  if (action.type === actions.SAVE_EMAIL) {
     return {
       ...state,
       email: action.payload,
@@ -142,56 +156,55 @@ const reducer = (state, action) => {
   }
 
   /* Preferences */
-  if (action.type === actionsModule.CHANGE_COLORIES) {
+  if (action.type === actions.CHANGE_CALORIES) {
     return {
       ...state,
       calories: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_DIET) {
+  if (action.type === actions.CHANGE_DIET) {
     return {
       ...state,
       diet: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_HEALTH) {
+  if (action.type === actions.CHANGE_HEALTH) {
     return {
       ...state,
       health: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_MEAL) {
+  if (action.type === actions.CHANGE_MEAL) {
     return {
       ...state,
       meal: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_CUISINE) {
+  if (action.type === actions.CHANGE_CUISINE) {
     return {
       ...state,
       cuisine: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_DISH) {
+  if (action.type === actions.CHANGE_DISH) {
     return {
       ...state,
       dish: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_MININPUT) {
+  if (action.type === actions.CHANGE_MININPUT) {
     return {
       ...state,
       minInput: action.payload,
     };
   }
-  if (action.type === actionsModule.CHANGE_MAXINPUT) {
+  if (action.type === actions.CHANGE_MAXINPUT) {
     return {
       ...state,
       maxInput: action.payload,
     };
   }
 
-  //   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
