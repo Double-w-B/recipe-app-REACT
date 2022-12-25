d742676d5ba62b39fa55d8a34a1dc92d09a31d40
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-12-25 23:50:37 +0100

    added logic to fetch particular saved recipe from DB

--- src/components/shared/Recipe/Recipe.js
+++ src/components/shared/Recipe/Recipe.js
@@ -1,87 +1,85 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import { AppContext } from "../../../context/context";
 import * as Component from "./index";
 import { useLocation } from "react-router-dom";
 import Navigation from "../Navigation";
 import StyledRecipe from "./style";
+import Loading from "../../Pages/Loading/Loading";
+import Error from "../../Pages/Error/Error";
 
 const Recipe = () => {
   const location = useLocation();
-  const { path, recipes, setRecipe, changePath } = useContext(AppContext);
-  const { localStrPath, newLocalStrPath } = useContext(AppContext);
-  const { updateLocalStrRecipes, localStrRecipes, rotateStar } =
-    useContext(AppContext);
-  const { lastQuery, recipesAmount } = useContext(AppContext);
+  const locationType = location.state.type;
+  const recipeID = location.state.id;
 
-  const [checked, setChecked] = useState(false);
+  const { recipes, lastQuery, recipesAmount } = React.useContext(AppContext);
+
+  const [isLoading, setIsLoading] = React.useState(setLoading());
+  const [fetchedRecipe, setFetchedRecipe] = React.useState("");
+  const found = locationType === "query" ? findRecipe() : fetchedRecipe;
 
   React.useEffect(() => {
     document.getElementById("root").scrollTo(0, 0);
-  });
 
-  const allRecipes = location.state === "query" ? recipes : localStrRecipes;
-
-  const found = allRecipes.find((item) => {
-    const { uri } = item.recipe;
-    if (location.state === "query") return uri.split("_")[1] === path;
-    return uri.split("_")[1] === localStrPath;
-  });
-
-  React.useEffect(() => {
-    if (location.state === "query") {
-      changePath(window.location.pathname);
-    }
+    if (locationType !== "query") findRecipeInDB();
     // eslint-disable-next-line
   }, []);
 
-  React.useEffect(() => {
-    if (location.state === "query") {
-      Object.values(localStrRecipes).find(
-        (item) =>
-          item.recipe.uri.split("_")[1] === found.recipe.uri.split("_")[1] &&
-          setChecked(true)
-      );
+  //! API Request - Start
+  const findRecipeInDB = async () => {
+    const apiUrl = `/api/v1/recipes/${recipeID}`;
+
+    try {
+      const response = await fetch(apiUrl, { method: "GET" });
+      const data = await response.json();
+
+      if (!response.ok) {
+        setFetchedRecipe("");
+        const timer = setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+      }
+
+      const timer = setTimeout(() => {
+        setFetchedRecipe(data);
+        setIsLoading(false);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    } catch (error) {
+      console.log(error);
     }
-  });
-
-  const checkStorage = (found) => {
-    const filteredStorage = () =>
-      Object.values(localStrRecipes).filter(
-        (item) =>
-          item.recipe.uri.split("_")[1] !== found.recipe.uri.split("_")[1]
-      );
-
-    if (location.state === "query" && checked) {
-      updateLocalStrRecipes(filteredStorage());
-      setRecipe("");
-      return;
-    }
-
-    if (location.state === "query" && !checked) {
-      updateLocalStrRecipes([...localStrRecipes, found]);
-      rotateStar();
-      return;
-    }
-
-    updateLocalStrRecipes(filteredStorage());
-    newLocalStrPath("removedRecipe");
   };
+  //! API Request - End
+
+  function findRecipe() {
+    return recipes.find((item) => item.recipe.uri.split("_")[1] === recipeID);
+  }
+
+  function setLoading() {
+    return locationType === "query" ? false : true;
+  }
+
   const checkPage = () => {
-    if (location.state === "query") {
+    if (locationType === "query") {
       return "query-recipe";
-    } else {
-      return "saved-recipe";
     }
+
+    return "saved-recipe";
   };
 
   const initialState = {
-    checked,
-    setChecked,
     found,
-    checkStorage,
   };
 
-  const { image } = found.recipe;
+  if (!found && !isLoading) {
+    return <Error />;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <>
@@ -89,15 +87,15 @@ const Recipe = () => {
         query={lastQuery}
         amount={recipesAmount}
         page={checkPage()}
-        title={found.recipe.label}
+        title={found?.recipe.label}
       />
       <StyledRecipe>
         <StyledRecipe.Container>
           <StyledRecipe.Image>
-            <img src={image} alt="" />
+            <img src={found.recipe.image} alt="" />
           </StyledRecipe.Image>
 
-          <Component.MainInfo {...initialState} type={location.state} />
+          <Component.MainInfo {...initialState} type={locationType} />
           <Component.Nutrition found={found} />
           <Component.Ingredients found={found} />
         </StyledRecipe.Container>
